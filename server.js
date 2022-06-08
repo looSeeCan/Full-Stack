285aa746e011bf363f098cfea1e95bf17faa4bb3
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-07 22:51:44 -0500

    Nyob Zoo
    Nyob Zoo on the Ui.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express       = require("express"),//define express. require it
+    cors           = require("cors");//define cors. require it
+
+const API_PORT = process.env.PORT || 5000;//define a port || this is or
+const app = express();
+
+
+app.use(cors());//allows us to connect our front end to our backend
+
+app.get("/api", function(req, res) {    
+    console.log("called");
+    res.send({result: "Helloooo"});
+}); 
+
+app.get("/quit", function(req, res) {    
+    console.log("called quit");
+    res.send({result: "Good Bye"});
+}); 
+
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
+//made a change in the package.json file: "proxy": localhost 5000
 function App() {
+  const [returnedData, setReturnedData] = useState(["Nyob Zoo"]);  
+  const getData = async(url) => {
+    const newData = await fetch(url, {
+      method: "GET",//all we are doing is grabbing from the front end and making it appear on the front end
+      headers: {//headers help us tell the response how we are sending and accepting data
+        "content-type": "aplication/json",
+        "Accept": "application/json"//we are accepting the data in a json format
+      }
+    })
+    .then(res => res.json());
+    console.log(newData);
+  };
+  
+  getData("/api");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {returnedData}
     </div>
   );
 }
