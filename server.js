f4b9a2c341fe1fc60647c2e11175be7da87ee3ca
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-10 18:36:03 -0500

    last vid
    trouble shooting the onClick buttons. they are not doing what they are supposed to be doing.

--- server.js
+++ server.js
@@ -2,28 +2,34 @@ const express       = require('express'),//define express. require it
     dbOperations    = require('./dbfiles/dbOperations'),//import
     cors           = require('cors'),//define cors. require it
     Employee        =require("./dbfiles/employee");
-// const API_PORT = process.env.PORT || 5000;//define a port || this is or
-// const app = express();
 
-// app.use(cors());//allows us to connect our front end to our backend
+const API_PORT = process.env.PORT || 5000;//define a port || this is or
+const app = express();
 
-// app.get("/api", function(req, res) {    
-//     console.log("called");
-//     res.send({result: "Helloooo"});
-// }); 
+//define some variables for mongoDB
+let client;
+let session;
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
 
-// app.get("/quit", function(req, res) {
-//     console.log("called quit");
-//     res.send({result: "Good Bye"});
-// }); 
+app.post("/api", function(req, res) {    
+    console.log("called");
+    res.send({result: "Helloooo"});
+}); 
+
+app.post("/quit", function(req, res) {
+    console.log("called quit");
+    res.send({result: "Good Bye"});
+}); 
 
 let Pam = new Employee(1002, "Pam", "Beazley", 29, "Female");
 // console.log(Pam);
 
-dbOperations.getEmployees().then(res => {
-    console.log(res.recordset);//an array of objects. our only object now though is jim halpert
-});
+// dbOperations.getEmployees().then(res => {
+//     console.log(res.recordset);//an array of objects. our only object now though is jim halpert
+// });
 
-dbOperations.createEmployees(Pam);
+// dbOperations.createEmployees(Pam);
 
-// app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
--- src/App.js
+++ src/App.js
@@ -4,23 +4,80 @@ import React, {useState} from "react";
 //made a change in the package.json file: "proxy": localhost 5000
 function App() {
   const [returnedData, setReturnedData] = useState(["Nyob Zoo"]);  
+  
+  //capture the input fields below as they are being typed
+  const [employee, setEmployee] = useState({EmployeeID: 0, FirstName: "", LastName: "",  Age: 0, Gender: ""});
+  // console.log(employee);
+  
+  const setInput = (e) => {
+    const {name, value} = e.target;
+    // console.log(name);
+    // console.log(value);
+    // console.log(e.target);
+    if (name === "EmployeeID" || name === "Age") {//if these two then 
+      setEmployee(prevState => ({
+        ...prevState,
+        [name]: parseInt(value)//parseInt. takes a string and cnverts it to a number
+      }));
+      return;//return early. you no longer need the else statement. if the condition is true then the return. Nothing else executes after return
+    }
+    setEmployee(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   const getData = async(url) => {
-    const newData = await fetch(url, {
-      method: "GET",//all we are doing is grabbing from the front end and making it appear on the front end
-      headers: {//headers help us tell the response how we are sending and accepting data
-        "content-type": "aplication/json",
-        "Accept": "application/json"//we are accepting the data in a json format
-      }
-    })
-    .then(res => res.json());
-    console.log(newData);
-    //So how do we make the api call, store that value in returned data, then produce that change on the UI
-    setReturnedData(newData.result);
+    console.log(employee);
+    // const newData = await fetch(url, {
+    //   method: "GET",//all we are doing is grabbing from the back end and making it appear on the front end
+    //   headers: {//headers help us tell the response how we are sending and accepting data
+    //     "content-type": "aplication/json",
+    //     "Accept": "application/json"//we are accepting the data in a json format
+    //   }
+    // })
+    // .then(res => res.json());
+    // console.log(newData);
+    // //So how do we make the api call, store that value in returned data, then produce that change on the UI
+    // setReturnedData(newData.result);
   };
   
   return (
     <div className="App">
-      <button onClick={() => getData("/quit")}>Click</button>
+      <input 
+        type= "number"//attribute to only accept numbers
+        name='EmplyoyeeID'
+        placeholder='EmployeeID'
+        onChange={setInput}></input>
+      
+      <input
+      name='FirstName'
+      placeholder='FirstName'
+      onChange={setInput}></input>
+      
+      <input
+      name='LastName'
+      placeholder='LastName'
+      onChange={setInput}></input>
+
+      <input
+      type= "number"
+      name='Age'
+      placeholder='Age'
+      onChange={setInput}></input>
+      
+      <input
+      name='Gender'
+      placeholder='Gender'
+      onChange={setInput}></input>
+
+      <button onClick={() => getData()}>Click</button>
+      <button onClick={() => getData("/quit")}>Create</button>
+      <p>EmployeeID: {returnedData.EmployeeID}</p>
+      <p>FirstName: {returnedData.FirstName}</p>
+      <p>LastName: {returnedData.LastName}</p>
+      <p>Age: {returnedData.Age}</p>
+      <p>Gender: {returnedData.Gender}</p>
       {returnedData}
     </div>
   );
